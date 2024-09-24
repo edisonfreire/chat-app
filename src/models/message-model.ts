decb39c5d5fa7cb7b036cdaf39a6c7b566ab9753
Edison <edisonfreire14@gmail.com>
2024-09-24 18:32:51 -0400

    added chat-mode and message-model

---
+++ src/models/message-model.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  chatId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "chats",
+    required: true,
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  text: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  readBy: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "users",
+  }
+}, { timestamps: true });
+
+if (mongoose.models && mongoose.models["messages"]) {
+  mongoose.deleteModel("messages");
+}
+
+const MessageModel = mongoose.model("messages", messageSchema);
+
+export default MessageModel;
