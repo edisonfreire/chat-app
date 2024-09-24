cecbebec2c5755ca60843a8bbf9d3e5c3578fac6
Edison <edisonfreire14@gmail.com>
2024-09-24 03:45:16 -0400

    header and content layout providers

---
+++ src/app/(private)/groups/create-group/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateGroupPage() {
+  return (
+    <div className="p-5">
+      <h1 className='text-sm'>Create Group</h1>
+    </div>
+  )
+}
+
+export default CreateGroupPage
