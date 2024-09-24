bfcdfc3d2620bfd49f5c1063308b7f18a1f37de5
Edison <edisonfreire14@gmail.com>
2024-09-24 03:57:37 -0400

    header component styling

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
   },
-  profilePic: {
+  profilePicture: {
     type: String,
     required: false,
   },
--- src/providers/layout-components/header.tsx
+++ src/providers/layout-components/header.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+"use client";
+import { GetCurrentUserFromMongoDB } from '@/server-actions/users';
+import React, { useEffect } from 'react'
+import { Avatar, message } from 'antd';
 
 function Header() {
+  const [currentUser, setCurrentUser] = React.useState<any>(null);
+
+  const getCurrentUser = async () => {
+    try {
+      const response = await GetCurrentUserFromMongoDB();
+      if (response.error) throw new Error(response.error);
+      setCurrentUser(response);
+    } catch (error: any) {
+      message.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getCurrentUser();
+  } ,[]);
+
   return (
-    <div className='bg-gray-200 w-full px-5 flex justify-between items-center'>
+    <div className='bg-gray-200 w-full px-5 py-2 flex justify-between items-center border-b border-solid border-gray-300'>
       <div>
-        <h1 className='text-2xl font-bold text-primary'>Chatster</h1>
+        <h1 className='text-2xl font-bold text-primary uppercase'>Chatster</h1>
       </div>
-      <div>
-        <span>Current user</span>
+      <div className='gap-5 flex items-center'>
+        <span className='text-sm'>{currentUser?.name}</span>
+        <Avatar src={currentUser?.profilePicture} />
       </div>
     </div>
   )
