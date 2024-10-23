e859cdd626bba23adb0b3a888cfdea73eb1243e2
Edison <edisonfreire14@gmail.com>
2024-10-23 19:43:24 -0400

    fix

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 //  mongod --dbpath /usr/local/var/mongodb
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL!);
+    await mongoose.connect(process.env.MONGO_URL!)
     console.log("MongoDB connected");
   } catch (error) {
-    console.log("MongoDB connection error", error);
+    console.log(error);
   }
 }
--- src/config/socket-config.ts
+++ src/config/socket-config.ts
@@ -1,4 +1,4 @@
-const { io } = require('socket.io-client');
+const { io } = require("socket.io-client");
 
 const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL);
 
