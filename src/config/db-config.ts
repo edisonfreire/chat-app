e0e449c738ec4a8053355c75e37046eb8134a9a5
Edison <edisonfreire14@gmail.com>
2024-10-23 22:34:55 -0400

    test mongo

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 //  mongod --dbpath /usr/local/var/mongodb
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL!)
+    await mongoose.connect('mongodb+srv://edisonfreire14:faXeK5hijgGJlvku@cluster0.4zc2s.mongodb.net/')
     console.log("MongoDB connected");
   } catch (error) {
     console.log('MongoDB connection error:', error);
