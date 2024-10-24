c23e0f1cb9a32460ac2811d84d2e713b326ed7da
Edison <edisonfreire14@gmail.com>
2024-10-23 22:23:39 -0400

    debugging

--- src/server-actions/users.ts
+++ src/server-actions/users.ts
@@ -1,4 +1,4 @@
-"use server"; // server action
+"use server";
 import { connectMongoDB } from "@/config/db-config";
 import UserModel from "@/models/user-model";
 import { currentUser } from "@clerk/nextjs/server";
@@ -11,51 +11,53 @@ export const GetCurrentUserFromMongoDB = async () => {
     // check if the user is already in the database based on clerkUserId
     const mongoUser = await UserModel.findOne({ clerkUserId: clerkUser?.id });
     if (mongoUser) {
-      // convert to normal objects to avoid warnings
-      return JSON.parse(JSON.stringify(mongoUser))
+      return JSON.parse(JSON.stringify(mongoUser));
     }
+
+    // if the user is not in the database, create a new user in the database
 
     let email = "";
     if (clerkUser?.emailAddresses) {
-      email = clerkUser?.emailAddresses[0]?.emailAddress || '';
+      email = clerkUser?.emailAddresses[0]?.emailAddress || "";
     }
 
-    // if the user is not in the database, create a user in the database
     const newUserPayload = {
       clerkUserId: clerkUser?.id,
       name: clerkUser?.firstName + " " + clerkUser?.lastName,
-      username: clerkUser?.username,
+      userName: clerkUser?.username,
       email,
       profilePicture: clerkUser?.imageUrl,
-    }
+    };
 
     const newUser = await UserModel.create(newUserPayload);
     return JSON.parse(JSON.stringify(newUser));
-  } catch (error: any) { // syntax for returning errors in server action catch block
+  } catch (error: any) {
     return {
-      error: error.message
+      error: error.message,
     };
   }
-}
+};
 
 export const UpdateUserProfile = async (userId: string, payload: any) => {
   try {
-    const updatedUser = await UserModel.findByIdAndUpdate(userId, payload, { new: true });
+    const updatedUser = await UserModel.findByIdAndUpdate(userId, payload, {
+      new: true,
+    });
     return JSON.parse(JSON.stringify(updatedUser));
   } catch (error: any) {
     return {
-      error: error.message
+      error: error.message,
     };
   }
-}
+};
 
 export const GetAllUsers = async () => {
   try {
-    const allUsers = await UserModel.find({});
-    return JSON.parse(JSON.stringify(allUsers));
+    const users = await UserModel.find({});
+    return JSON.parse(JSON.stringify(users));
   } catch (error: any) {
     return {
-      error: error.message
+      error: error.message,
     };
   }
 }
