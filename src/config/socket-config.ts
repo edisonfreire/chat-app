39f02d48173780961c21817cd403632d78710c35
Edison <edisonfreire14@gmail.com>
2024-10-23 20:21:59 -0400

    cors fix

--- src/config/socket-config.ts
+++ src/config/socket-config.ts
@@ -1,5 +1,8 @@
 const { io } = require("socket.io-client");
 
-const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL);
+const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL, {
+  withCredentials: true,
+  transports: ['websocket', 'polling']
+});
 
 export default socket;
